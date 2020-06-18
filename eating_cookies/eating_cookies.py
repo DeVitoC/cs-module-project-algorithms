2e054feebd5b6239fe8a638e5f049e22f1932c07
Christopher DeVito <christopher.devito@protonmail.com>
2020-06-18 12:05:05 -0400

    Implemented eating_cookies. passes both tests.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,20 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, optional = 0):
     # Your code here
+    prev1, prev2, prev3 = 1, 1, 0
+    combinations = 1
+    for i in range (1, n):
+        combinations = prev1 + prev2 + prev3
+        prev3, prev2, prev1 = prev2, prev1, combinations
 
-    pass
+    return combinations
+
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
