c123757f91f872ecea66be8411e0c1858fe2f6aa
Christopher DeVito <christopher.devito@protonmail.com>
2020-06-17 20:27:30 -0400

    implemented sliding_window_max. passes main test, but not large_input test

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,12 +4,13 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
-
-    pass
-
+    arr
+    for i in range(len(nums)-(k-1)):
+         arr.append(max(nums[i:i+k]))
+    return arr
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
